ad699ba059244966c7ec5434f83ef5bfccc03bb6
Umayanga Vidunuwan <73218580+Vidunuwan@users.noreply.github.com>
2024-01-06 01:05:50 +0530

    Last Stable update

--- Assets/Scripts/PlayerStat.cs
+++ Assets/Scripts/PlayerStat.cs
@@ -10,23 +10,5 @@ using Fusion.Sockets;
 
 public class PlayerStat : NetworkBehaviour
 {
-   //[Networked(OnChanged = nameof()] public NetworkString<_32> PlayerName { get; set; }
-    [SerializeField] TMP_Text playerNameLabel;
-
-    [Networked] public float Health { get; set; }
-
-
-    private void Start()
-
-    {
-        if (this.HasInputAuthority)
-        {
-           //PlayerName = FusionManager.instance._playername;
-        }
-       
-    }
-    /*protected static void UpdatePlayerName(Changed<PlayerStat> changed)
-    {
-        changed.Behaviour.playerNameLabel.text = changed.Behaviour.PlayerName.ToStrng();
-    }*/
+   
 }
